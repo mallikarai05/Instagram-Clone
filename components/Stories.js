f2f094d30e40fab6e20e10961cfbfaf00b6afab2
Mallika Rai <mallika.rai05@gmail.com>
2022-07-04 23:34:35 +0530

    Instagram Clone

---
+++ components/Stories.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { faker } from '@faker-js/faker';
+import Story from './Story';
+
+function Stories() {
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const suggestions = [...Array(20)].map(() => ({
+            userId: faker.datatype.uuid(),
+            username: faker.internet.userName(),
+            avatar: faker.image.avatar(),
+          }
+        ));
+        setSuggestions(suggestions);
+    }, [])
+
+    return (
+        <div className='flex space-x-2 p-6 bg-white
+           mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+            {suggestions.map(profile => (
+                <Story key={profile.userId}
+                       img={profile.avatar}
+                       username={profile.username} />
+            ))}
+
+        </div>
+    )
+}
+
+export default Stories
---
+++ components/Suggestions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Suggestions() {
+  return (
+    <div>
+      <h1>Suggestions</h1>
+    </div>
+  )
+}
+
+export default Suggestions
