90ea09d8779561513547001159749c50a1625c68
mallikarai05 <mallika.rai05@gmail.com>
2022-07-14 20:51:45 +0530

    Change nextauth.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  secret: NEXTAUTH_URL,
+  secret: process.env.NEXTAUTH_URL,
   pages: {
     signIn: '/auth/signin',
   },
